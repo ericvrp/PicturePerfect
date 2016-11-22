41d5810c9e5343729e90ecec5fd8acdcb628a901
root <ericvrp@gmail.com>
2016-11-22 15:33:45 -0500

    Initialy use thumbnail instead of large external image

--- src/Image.js
+++ src/Image.js
@@ -8,16 +8,18 @@ class Image extends Component {
 
     const {albumNum = 0} = this.props.params
     const {pictureNum = 0} = this.props.params
-    const image = albums[albumNum].pictures[pictureNum].image
+    const {image, thumbnail} = albums[albumNum].pictures[pictureNum]
 
     const scale = 0.95 * Math.min(window.innerWidth / image.width, window.innerHeight / image.height)
 
     // document.body.style.backgroundImage = "url(" + image.link + ")"
 
+    console.log(this.refs)
+
     return (
       <div>
-        <img src={image.link} className='Blurred Image' alt='' width={window.innerWidth} height={window.innerHeight} />
-        <img src={image.link} className='Image' alt='' width={image.width * scale} height={image.height * scale} />
+        <img src={thumbnail.link} className='Blurred Image' alt='' width={window.innerWidth} height={window.innerHeight} />
+        <img src={thumbnail.link} className='Image' alt='' ref='image' width={image.width * scale} height={image.height * scale} />
       </div>
     )
   }
