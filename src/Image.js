ea984541ca447296b63118dffb423a6259dd346f
Eric van Riet Paap <ericvrp@gmail.com>
2016-11-26 15:38:56 +0100

    More bright red shadow hilites

--- src/Image.js
+++ src/Image.js
@@ -5,21 +5,11 @@ import albums from './all-albums'
 // const hiresImage = new Image()
 
 export default class extends Component {
-  // onImageLoaded = () => {
-  //   // console.log('onImageLoaded')
-  //   // console.log(this.refs.image)
-  //   this.refs.image.src = hiresImage.src
-  // }
-
   render() {
     const {albumNum = 0} = this.props.params
     const {pictureNum = 0} = this.props.params
     const {image, thumbnail} = albums[albumNum].pictures[pictureNum]
     const scale = 0.95 * Math.min(window.innerWidth / image.width, window.innerHeight / image.height)
-
-    // load hires version so we can replace the thumbnail asap
-    // hiresImage.onload = this.onImageLoaded
-    // hiresImage.src = image.link
 
     return (
       <div onClick={this.props.router.goBack} onTouchEnd={this.props.router.goBack} width={image.width} height={image.height}>
