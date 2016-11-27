77551089a264d689edab1fc659344038414883df
Eric van Riet Paap <ericvrp@gmail.com>
2016-11-27 08:31:42 +0100

    Remove onTouchEnd once more because Chrome on Android then handles the event twice

--- src/Image.js
+++ src/Image.js
@@ -10,7 +10,7 @@ export default class extends Component {
     const scale = 0.95 * Math.min(window.innerWidth / image.width, window.innerHeight / image.height)
 
     return (
-      <div onTouchEnd={this.props.router.goBack}>
+      <div>
         <img onClick={this.props.router.goBack} src={thumbnail.link} className='CenterBothDirections Dimmed Blurred' alt='' width={window.innerWidth} height={window.innerHeight} />
         <ProgressiveImage onClick={this.props.router.goBack} src={image.link} lowresSrc={thumbnail.link} className='CenterBothDirections Image' width={image.width * scale} height={image.height * scale} />
       </div>
