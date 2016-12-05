802fd2ec605e81d273af7d68ab6414cf101bc72e
Eric van Riet Paap <ericvrp@gmail.com>
2016-12-05 22:24:01 +0100

    Try 5

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import 'react-fastclick'
 import FullScreen from 'react-fullscreen';
 import './App.css'
 
-import inobounce from 'inobounce'
-inobounce.enable()
-// console.log(inobounce.isEnabled())
-// document.body.addEventListener('touchMove', function(event) {
-//   event.preventDefault();
-//   console.log('touchmove preventDefault')
-// });
-
 export default class extends Component {
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import Album from './Album'
 import Image from './Image'
 import NotFound from './NotFound'
 
+import inobounce from 'inobounce'
+inobounce.enable()
+// console.log(inobounce.isEnabled())
+document.body.addEventListener('touchMove', function(event) {
+  event.stopPropagation()
+// console.log('touchmove stopPropagation')
+})
+
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path='/' component={App}>
