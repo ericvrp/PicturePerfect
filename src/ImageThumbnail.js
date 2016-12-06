cee22e3e02fb0d98241482fbd726c736eba749bc
Eric van Riet Paap <ericvrp@gmail.com>
2016-12-06 13:34:50 +0100

    Use image instead of thumbnail

--- src/ImageThumbnail.js
+++ src/ImageThumbnail.js
@@ -4,12 +4,12 @@ import { browserHistory } from 'react-router'
 export default class extends Component {
   render() {
     const {album, albumNum, index, imageHeight} = this.props
-    const {thumbnail} = album.pictures[index % album.pictures.length]
+    const {thumbnail, image} = album.pictures[index % album.pictures.length]
     const scale = imageHeight / thumbnail.height
     const width = parseInt(thumbnail.width * scale, 10)
     // console.log(scale, thumbnail.link)
     return (
-      <img onClick={browserHistory.push.bind(this, '/image/' + albumNum + '/' + index)} src={thumbnail.link} alt='' className='ImageThumbnail' width={width} height={imageHeight} />
+      <img onClick={browserHistory.push.bind(this, '/image/' + albumNum + '/' + index)} src={image.link} alt='' className='ImageThumbnail' width={width} height={imageHeight} />
     )
   }
 }
