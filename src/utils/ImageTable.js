d83da595b7d3698150ae922e34d727ab509fdd4f
Eric van Riet Paap <ericvrp@gmail.com>
2016-12-08 16:17:50 +0100

    Make ImageTable reactive to screensize changes

--- src/utils/ImageTable.js
+++ src/utils/ImageTable.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react'
-import { List } from 'react-virtualized'
+import React, {Component} from 'react'
+import {List} from 'react-virtualized'
 import ScreenSize from './ScreenSize'
-
 
 const NumberOfColumns = (nPictures, startIndex, preferredNumColumns) => {
   return Math.min(preferredNumColumns, nPictures - startIndex)
@@ -24,18 +23,32 @@ const ImageHeight = (album, startIndex, numberOfColumns, margin) => {
 
 const range = n => {
   const a = []
-  for (let i = 0; i < n; i++) a.push(i)
+  for (let i = 0; i < n; i++) 
+    a.push(i)
   return a
 }
 
-const ImageRow = ({imageComponent, album, albumNum, startIndex, numberOfColumns, margin, style}) => {
+const ImageRow = ({
+  imageComponent,
+  album,
+  albumNum,
+  startIndex,
+  numberOfColumns,
+  margin,
+  style
+}) => {
   const imageHeight = ImageHeight(album, startIndex, numberOfColumns, margin)
   const realStartIndex = startIndex % album.pictures.length
   const ImageComponent = imageComponent // because React needs a starting capital for JSX tags
   // console.log('ImageRow', startIndex, imageHeight, style)
   return (
     <div className='ImageTableRow' style={style}>
-      {range(numberOfColumns).map((_, columnIndex) => <ImageComponent album={album} albumNum={albumNum} index={realStartIndex + columnIndex} imageHeight={imageHeight} key={realStartIndex + columnIndex} />)}
+      {range(numberOfColumns).map((_, columnIndex) => <ImageComponent
+        album={album}
+        albumNum={albumNum}
+        index={realStartIndex + columnIndex}
+        imageHeight={imageHeight}
+        key={realStartIndex + columnIndex}/>)}
     </div>
   )
 }
@@ -44,8 +57,12 @@ export default class extends Component {
   constructor(props) {
     super(props)
 
-    this._getRowHeight = this._getRowHeight.bind(this)
-    this._rowRenderer = this._rowRenderer.bind(this)
+    this._getRowHeight = this
+      ._getRowHeight
+      .bind(this)
+    this._rowRenderer = this
+      ._rowRenderer
+      .bind(this)
   }
 
   _getRowHeight({index}) {
@@ -61,32 +78,55 @@ export default class extends Component {
 
   _rowRenderer({index, key, style}) {
     const rowIndex = index
-    // console.log('_rowRenderer', rowIndex, key, style)
-    // console.log(ScreenSize())
+    // console.log('_rowRenderer', rowIndex, key, style) console.log(ScreenSize())
 
-    const {imageComponent, album, albumNum, preferredNumColumns, margin, nRepeats} = this.props
+    const {
+      imageComponent,
+      album,
+      albumNum,
+      preferredNumColumns,
+      margin,
+      nRepeats
+    } = this.props
     const startIndex = preferredNumColumns * rowIndex
     const numberOfColumns = NumberOfColumns(album.pictures.length * nRepeats, startIndex, preferredNumColumns, margin)
 
-    return (
-      <ImageRow imageComponent={imageComponent} album={album} albumNum={albumNum} startIndex={startIndex} numberOfColumns={numberOfColumns} margin={margin} key={key} style={style} />
-    )
+    return (<ImageRow
+      imageComponent={imageComponent}
+      album={album}
+      albumNum={albumNum}
+      startIndex={startIndex}
+      numberOfColumns={numberOfColumns}
+      margin={margin}
+      key={key}
+      style={style}/>)
   }
 
   render() {
     const rowCount = parseInt((this.props.album.pictures.length * this.props.nRepeats + this.props.preferredNumColumns - 1) / this.props.preferredNumColumns, 10)
+    const estimatedRowSize = this._getRowHeight({index: 0})
+    // console.log('estimatedRowSize', estimatedRowSize)
+
+    if (this.refs.List) {
+      // console.log(this.refs.List)
+      this
+        .refs
+        .List
+        .recomputeRowHeights()
+    }
+
     return (
       <div className='ImageTable'>
         <List
           ref='List'
+          estimatedRowSize={estimatedRowSize}
           width={ScreenSize().width}
           height={ScreenSize().height}
           rowHeight={this._getRowHeight}
           overscanRowCount={5}
           rowCount={rowCount}
           rowRenderer={this._rowRenderer}
-          scrollToIndex={parseInt(rowCount / 2, 10)}
-        />
+          scrollToIndex={parseInt(rowCount / 2, 10)}/>
       </div>
     )
   }
